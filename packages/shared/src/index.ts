ae7e021efe66f020d4b6060aa0f1bd584759a2bc
mia-riezebos <mia@mia.cx>
2026-09-11 14:03:01 +0200

    docs(userscript): note the rebindable shortcuts
    Implements TODO: Changeset, design notes, full validation, and browser inspection of the settings section and help dialog.
    
    Closes #354

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -31,6 +31,7 @@ export {
   quantiseToPalette,
 } from './quantise.js'
 export * from './region-shape.js'
+export * from './shortcuts.js'
 export {
   type PixelBounds,
   SliceError,
@@ -39,7 +40,6 @@ export {
   sliceTemplateForSurface,
   type TemplateChunk,
 } from './slice.js'
-export * from './shortcuts.js'
 export * from './slug.js'
 export * from './tags.js'
 export * from './telemetry.js'
